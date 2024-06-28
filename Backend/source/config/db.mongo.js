dab303a98f3e4c106cbd5e763e244c8bcf03a2fe
DFacundoPerezN <3607834730101@ingenieria.usac.edu.gt>
2024-06-27 19:21:06 -0600

    Se realizaron arreglos para que docker-compose levantara todo

--- Backend/source/config/db.mongo.js
+++ Backend/source/config/db.mongo.js
@@ -1,112 +1,107 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const{
-    MONGO_USERNAME,
-    MONGO_PASSWORD,
-    MONGO_HOSTNAME,
-    MONGO_HOST,
-    MONGO_DATABASE
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_HOST,
+  MONGO_PORT,
+  MONGO_DATABASE
 } = process.env;
 
-//const uri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_HOST}/?authSource=${MONGO_DATABASE}`;
-const uri = 'mongodb://root:Proyecto2@localhost:27017';
+const uri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}?authSource=admin`;
 
-
-const insertData = async(database, data) => {
-    console.log('uri', uri);
-    const mongoClient = new MongoClient(uri);
-    try {
-        await mongoClient.connect();
-        const dbmongo = mongoClient.db('Usuarios');
-        const coleccion = dbmongo.collection(database);
-        const result = await coleccion.insertOne(data);
-        return result;
-    } catch (error) {
-        console.error('Error insertData: ', error);
-        return error;
-    } finally {
-        await mongoClient.close();
-    }
+const insertData = async (database, data) => {
+  console.log('uri', uri);
+  const mongoClient = new MongoClient(uri);
+  try {
+    await mongoClient.connect();
+    const dbmongo = mongoClient.db(MONGO_DATABASE);
+    const coleccion = dbmongo.collection(database);
+    const result = await coleccion.insertOne(data);
+    return result;
+  } catch (error) {
+    console.error('Error insertData: ', error);
+    return error;
+  } finally {
+    await mongoClient.close();
+  }
 };
 
-const comprobarData = async(database, data) => {
-    console.log('uri', uri);
-    const mongoClient = new MongoClient(uri);
-    try {
-        await mongoClient.connect();
-        const dbmongo = mongoClient.db('Usuarios');
-        const coleccion = dbmongo.collection(database);
-        const result = await coleccion.findOne(data);
+const comprobarData = async (database, data) => {
+  console.log('uri', uri);
+  const mongoClient = new MongoClient(uri);
+  try {
+    await mongoClient.connect();
+    const dbmongo = mongoClient.db(MONGO_DATABASE);
+    const coleccion = dbmongo.collection(database);
+    const result = await coleccion.findOne(data);
 
-        console.log('Resultado de la busqueda: ', result);
-        if(result !== null){
-
-            console.log('Contraseña correcta');
-            return result;
-        }
-        console.log('Contraseña y/o usuario incorrecto');
-        return result;
-
-    } catch (error) {
-        console.error('Error comprobarData: ', error);
-        return error;
-    } finally {
-        await mongoClient.close();
+    console.log('Resultado de la busqueda: ', result);
+    if (result !== null) {
+      console.log('Contraseña correcta');
+      return result;
     }
+    console.log('Contraseña y/o usuario incorrecto');
+    return result;
+
+  } catch (error) {
+    console.error('Error comprobarData: ', error);
+    return error;
+  } finally {
+    await mongoClient.close();
+  }
 };
 
-const deleteData = async(database, data) => {
-    console.log('uri', uri);
-    const mongoClient = new MongoClient(uri);
-    try {
-        await mongoClient.connect();
-        const dbmongo = mongoClient.db('Usuarios'); //Usuarios representa la base de datos
-        const coleccion = dbmongo.collection(database);
-        const result = await coleccion.findOneAndDelete(data);
-        //console.log('Se busco: ', data);
-        console.log('Resultado de la busqueda: ', result);
-        if(result !== null){
-            return result;
-        }
-        console.log('No se encontró con los datos proporcionados');
-        return result;
-
-    } catch (error) {
-        console.error('Error deleteData: ', error);
-        return error;
-    } finally {
-        await mongoClient.close();
+const deleteData = async (database, data) => {
+  console.log('uri', uri);
+  const mongoClient = new MongoClient(uri);
+  try {
+    await mongoClient.connect();
+    const dbmongo = mongoClient.db(MONGO_DATABASE);
+    const coleccion = dbmongo.collection(database);
+    const result = await coleccion.findOneAndDelete(data);
+    console.log('Resultado de la busqueda: ', result);
+    if (result !== null) {
+      return result;
     }
+    console.log('No se encontró con los datos proporcionados');
+    return result;
+
+  } catch (error) {
+    console.error('Error deleteData: ', error);
+    return error;
+  } finally {
+    await mongoClient.close();
+  }
 };
 
-const getData = async(database) => {
-    console.log('uri', uri);
-    const mongoClient = new MongoClient(uri);
-    try {
-        await mongoClient.connect();
-        const dbmongo = mongoClient.db('Usuarios'); //Usuarios representa la base de datos
-        const coleccion = dbmongo.collection(database);
-        const result = await coleccion.find().toArray();
-        //console.log('Resultado de la busqueda: ', result);
-        if(result !== null){
-            return result;
-        }
-        console.log('No se encontró con los datos proporcionados');
-        return result;
-
-    } catch (error) {
-        console.error('Error getData: ', error);
-        return error;
-    } finally {
-        await mongoClient.close();
+const getData = async (database) => {
+  console.log('uri', uri);
+  const mongoClient = new MongoClient(uri);
+  try {
+    await mongoClient.connect();
+    const dbmongo = mongoClient.db(MONGO_DATABASE);
+    const coleccion = dbmongo.collection(database);
+    const result = await coleccion.find().toArray();
+    if (result !== null) {
+      return result;
     }
-}
+    console.log('No se encontró con los datos proporcionados');
+    return result;
 
-module.exports ={
-    insertData,
-    comprobarData,
-    deleteData, 
-    getData
+  } catch (error) {
+    console.error('Error getData: ', error);
+    return error;
+  } finally {
+    await mongoClient.close();
+  }
+};
+
+module.exports = {
+  insertData,
+  comprobarData,
+  deleteData,
+  getData
 };
